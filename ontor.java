8b1656fae833c4413c9548c85e6d439931624636
Ontor-Hazari <ontorhazary570@gmil.com>
2021-09-23 13:04:29 +0600

    This ontor.java file now update and add new functaion ( calling , reciver peramiter string)

--- ontor.java
+++ ontor.java
@@ -3,5 +3,12 @@ class ontor
     public static void main(String[] args)
     {
         System.out.println("Hello This is second git project");
+
+        calling("Ontor Hazari");
+    }
+
+    void calling(String s)
+    {
+        System.out.println("Her Name Is : "+s);
     }
 }
